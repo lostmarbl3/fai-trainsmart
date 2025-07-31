b05982cda89dff8ddcdac627ae48eabee051de17
Lostmarbl3 <ermontgo@gmail.com>
2025-07-31 15:49:47 -0500

    Remove legacy dashboard route, redirect to /trainer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import TrainerDashboard from "./pages/TrainerDashboard";
 import ClientDashboard from "./pages/ClientDashboard";
@@ -12,6 +12,8 @@ const App = () => {
         <Route path="/trainer" element={<TrainerDashboard />} />
         <Route path="/client" element={<ClientDashboard />} />
         <Route path="/solo" element={<SoloDashboard />} />
+        {/* Redirect legacy /dashboard route to Trainer view */}
+        <Route path="/dashboard" element={<Navigate to="/trainer" replace />} />
       </Routes>
     </Router>
   );
